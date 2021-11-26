716cbadcd3902eed823baf2dea71fefbb46b56a7
richsoufienne <rich.soufien@gmail.com>
2021-11-26 16:15:13 +0100

    add job views & url

--- job/admin.py
+++ job/admin.py
@@ -4,10 +4,11 @@ from django.contrib import admin
 
 
 
-from .models import Job 
+from .models import Job, Category 
 
 
 admin.site.register(Job)
+admin.site.register(Category)
 
 
 
---
+++ job/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    
+    path('',views.job_list , name='job_list'),
+   
+    path('<int:id>',views.job_detail , name='job_detail'),
+]
